ddd6506e718fb514445e6caa177aefc8d8e4711a
samuel gracian <156386408+SamuelGracian@users.noreply.github.com>
2026-09-04 19:03:56 -0600

    Feat/pre rec utilities (#2)
    * Create BowPlatformDefines
    
    * Platform defines, and folder architecture
    
    * Fixed some typos and missing path
    
    * update Platform Types
    
    * Add platform math module to bowUtilities
    
    Introduces a new platform math layer with `PlatformMath::cos` and wires it into the utilities library via new `math`/`platform` headers and source files. Updates `CMakeLists.txt` to build from recursive source/header globs and expose the new include directories, removes the unused empty `main.cpp`, and fixes a GCC detection macro typo in platform defines.
    
    * update platform types
    
    ---------
    
    Co-authored-by: Accel <accel.mr@gmail.com>

---
+++ bowUtilities/include/platform/bowPlatformDefines.h
@@ -0,0 +1,308 @@
+/************************************************************************/
+/**
+ * @file BowPlatformDefines.h
+ * @author Samuel Gracian
+ * @date 03/09/2026
+ * @brief Basic definitions to describe target platform.
+ *
+ * This definitions are meant to describe the target platform.
+ *
+ * @bug No bug known.
+ */
+ /************************************************************************/
+#pragma once
+
+/************************************************************************/
+/**
+ * Initial platform / compiler-related stuff to set
+ */
+/************************************************************************/
+#define BOW_PLATFORM_WIN32   1           //Windows platform
+#define BOW_PLATFORM_LINUX   2           // Linux Platform
+#define BOW_PLATFORM_OSX     3
+
+
+#define BOW_COMPILER_MSVC    1            //Visual Studio Compiler
+#define BOW_COMPILER_GNUC    2            //GCC Compiler
+#define BOW_COMPILER_INTEL   3            //Intel Compiler
+#define BOW_COMPILER_CLANG   4            //Clang Compiler
+
+#define BOW_ARCHITECTURE_X86_32 1         //Intel x86 32 bits
+#define BOW_ARCHITECTURE_X86_64 2         //Intel x86 64 bits
+
+#define BOW_ENDIAN_LITTLE  1
+#define BOW_ENDIAN_BIG     2
+
+//Define the actual endian typpe (little enian type for windows, linux, Apple and ps4?)
+#define BOW_ENDIAN BOW_ENDIAN_LITTLE
+#define BOW_VERSION_MAJOR    0                   //Engine version major
+#define BOW_VERSION_MINOR    1
+#define BOW_VERSION_PATCH    0
+#define BOW_VERSION_BUILD    1
+
+//define if on create we want to report warnings on unkown symbols
+#define BOW_DEBUG_DETAILED_SYMBOLS   1
+
+/************************************************************************/
+/*
+ *Compiler type and version
+ */
+/************************************************************************/
+
+#if defined (__clang__)
+#   define BOW_COMPILER BOW_COMPILER_CLANG
+#   define BOW_COMPILER_VER __cland_version__
+#   define BOW_THREADLOCAL __trhead
+#   define BOW_STDCALL __attribute__((stdcall))
+#   define BOW_CDECL __attribute __((cdecl))
+#   define BOW_FALLTHROUHG [[clang::fallthrough]];
+#elif defined (__GNUC__) //Check after Cland, as Clang defines this too
+#   define BOW_COMPILER BOW_COMPILER_GNUC
+#   define BOW_COMPILER_VER (((__GNUC__)*100)+(__GNUC_MINOR__*10)+__GNUC_PATCHLEVEL__)
+#   define BOW_THREADLOCAL __thread
+#   define BOW_STDCALL __attribute__ ((stdcall))
+#   define BOW_CDECL __attribute__ ((cdecl))
+#   define BOW_FALLTHROHG __attribute__ ((fallthrough))
+#elif defined (__INTEL_COMPILER) //???
+#   define BOW_COMPILER BOW_COMPILER_INTEL
+#   define BOW_COMP_VER __INTEL_COMPILER
+#   define BOW_STDCALL __stdcall
+#   define BOW_CDECL __cdecl
+#   define BOW_FALLTHROUHG //???
+
+/**
+ * CH_THREADLOCAL define is down below because INTEL compiler defines it 
+ * differently based on platform 
+*/
+
+//Check after Clang and intel, we could biuld with VS
+#elif defined (_MSC_VER)
+#   define BOW_COMPILER BOW_COMPILER_MSVC
+#   define BOW_COMP_VER _MSC_VER
+#   define BOW_THREADLOCAL __declspec
+#   define BOW_STDCALL __stdcall
+#   define BOW_FALLTHOUHG
+#   undef __PRETTY_FUNCTION__
+#   define __PRETTY_FUNCTION__ __FUNCSIG__
+#else
+#   pragma error "No known computer"
+#endif
+
+/************************************************************************/
+/**
+ * See if we can use __forceinline or if we need to use __inline instead
+ */
+/************************************************************************/
+
+#if BOW_COMPILER == BOW_COMPILER_MSVC
+# if BOW_COMP_VER >= 1200
+#   define FORCELINE __forceline
+#   ifndef RESTRICT __restrict
+#   endif
+#endif
+#elif defined (__MINGW32__)
+# if !defined (FORCELINE)
+#   define FORCELINE __inline
+#   ifndef RESTRICT 
+#       define RESTRICT
+#   endif
+#endif
+#else
+#define FORCELINE __inline
+#ifndef RESTRICT
+#   define  RESTRICT
+#endif
+#endif
+
+/************************************************************************/
+/**
+ * Find the current platform
+ */
+/************************************************************************/
+#if defined (__WIN32__) || defined (_WIN32)
+# define BOW_PLATFORM BOW_PLATFORM_WIN32
+#elif defined (__APPLE_CC__)
+# define BOW_PLATFORM BOW_PLATFORM_OSX
+#elif defined (__ORBIS__)
+# define BOW_PLATFORM BOW_PLATFORM_LINUX
+#endif
+
+/************************************************************************/
+/**
+ * Find architecture type
+ */
+/************************************************************************/
+#if defined (__x86_x64__) || defined (_M_X64) // compile if is x64
+# define BOW_ARCH_TYPE BOW_ARCHITECTURE_X86_64
+#else 
+#  define BOW_ARCH_TYPE BOW_ARCHITECTURE_X86_32 
+#endif
+
+/************************************************************************/
+/**
+ * Memory aligment macros
+ */
+/************************************************************************/
+#if  BOW_COMPILER == BOW_COMPILER_MSVC  
+#   define ALIGN_AS(n)  __declspec(align(n))
+#elif BOW_COMPILER == BOW_COMPILER_GNUC || BOW_COMPILER == BOW_COMPILER_CLANG
+#   define ALIGN_AS(n) __attribute__((aligned(n)))
+#endif
+
+/************************************************************************/
+/**
+ * For throw override (deprecated on c++ 11 bus vs does not have handle)
+ */
+/************************************************************************/
+#if BOW_COMPILER == BOW_COMPILER_MSVC
+# define _NOEXCEPT noexcept
+#elif BOW_COMPILER == BOW_COMPILER_INTEL
+# define _NOEXCEPT noexcept
+#elif BOW_COMPILER == BOW_COMPILER_GNUC
+# define _NOEXCEPT noexcept
+#else
+#define _NOEXCEPT
+#endif
+
+/************************************************************************/
+/**
+ * Library export specifics
+ */
+/************************************************************************/
+#if BOW_PLAFORM == BOW_PLATFORM_WIN32
+# if BOW_COMPILER == BOW_COMPILER_MSVC
+#   if defined (BOW_STATIC_LIB)
+#       define BOW_UTILITY_EXPORT
+#   else
+#       if defiend (BOW_UTILITY_EXPORTS)
+#           define BOW_UTILTY_EXPORT __declspec(dllexport)
+#   else
+#       define BOW_UTILITY_EXPORT __declspec(dllimport)
+#       endif
+#   endif
+#else //Any other compiler
+#   if defined (BOW_STATIC_LIB)
+#       define BOW_UTILITY_EXPORT
+#   else
+#       if defined (BOW_UTILITY_EXPORTS)
+#           define BOW_UTILITY_EXPORT __attitude__ ((dllexport))
+#       else
+#           define BOW_UTILITY_EXPORT __attitude__ ((dllimport))
+#       endif
+#   endif
+#endif
+# define BOW_UTILITY_HIDDEN
+#else
+# define BOW_UTILITY_EXPORT __attitude__ ((visibility ("default")))
+# define BOW_UTILITY_HIDDEN __attitude__ ((visibility("hidden")))
+#endif
+
+//DLL export plg ins
+#if BOW_PLATFORM == BOW_PLATFORM_WIN32
+# if BOW_COMPILER == BOW_COMPILER_MSVC
+#   define BOW_PLUGIN_EXPORT __declspec (dllexport)
+# else
+#   define BOW_PLUGIN_EXPORT __attitude__ ((dllexport))
+# endif
+#else
+#   define BOW_PLUGIN_EXPORT __attitude__ ((visibility("default")))
+#endif
+
+/************************************************************************/
+/**
+ * Windows specific settings
+ */
+/************************************************************************/
+//WIN32
+# if defined (_DEBUG) || defined (DEBUG)
+#   define BOW_DEBUG_MODE 1
+#else
+#   define BOW_DEBUG_MODE 0
+# endif
+#if BOW_PLATFORM == BOW_PLATFORM_WIN32
+# if BOW_COMPILER == BOW_COMPILER_INTEL
+#   define BOW_TRHEADLOCAL __declspec(thread)
+# endif
+#endif //BOWIE PLATFORM
+
+/************************************************************************/
+/**
+ * Linux aplle specifc settings
+ */
+/************************************************************************/
+#if BOW_PLATFORM == BOW_PLATFORM_LINUX || BOW_PLATFORM == BOW_PLATFORM_OSX
+
+// If we're in debug mode 
+# if defined (_DEBUG) || deined (DEBUG)
+#   define BOW_DEBUG_MODE 1
+# else
+#   define BOW_DEBUG_MODE 0
+# endif 
+# if BOW_COMPILER == BOW_COMPILER_INTEL
+#   define BOW_THREADLOCAL thread
+# endif
+#endif //BOWIE PLATFORM
+
+/************************************************************************/
+/**
+ * Definition of debug macros
+ */
+/************************************************************************/
+#if BOW_DEBUG_MODE
+# define BOW_DEBUG_ONLY (x) x
+# define BOW_ASSERT (x) assert (x)
+#else
+# define BOW_DEBUG_ONLY (x)
+# define BOW_ASSERT (x)
+#endif
+
+/************************************************************************/
+/**
+ * Extern
+ */
+/************************************************************************/
+#define BOW_EXTERN extern "C"
+
+/************************************************************************/
+/**
+ * Disable some compiler warnings
+ */
+/************************************************************************/
+
+//If were compling on visual studio
+# if BOW_COMPILER == BOW_COMPLER_MSVC
+
+//Secure version aren't multi platform, so we won't be using them
+# define _CRT_SECURE_NO_WARNINGS
+
+/**
+ * Disable: nonstandard extension used: nameless struct/union
+     *  Happens when a struct or union is defined without a name
+ */ 
+/************************************************************************/
+#pragma warning (disable : 4201)
+
+/**
+ * Disable ",type> needs to ahve DLL interface to be used by clients"
+ * happens on STL member varaiables witch are not public therefore is ool
+ */
+#pragma warning (disable : 4251)
+
+//Disable 'X' Function call with parameters that may be unsafe
+#pragma warning (disable : 4996)
+
+/**
+ * Disable decorated name lenght exceeded, name was truncated. Happens with really long type names.
+ * Even fairly standards use for std::unordered_map with custom parameteers, meaning I can't really do much
+ * to avoid it.
+ * It shopuldn't affect execution but might casue problems if u compile library with one compiler and use it in another
+ */
+#pragma warning (disable : 4503)
+
+   /**
+    * Disable: nonstandard extension used: override specifier 'keyword'.
+    * Happens when a keyword was used that is not in the C++ standard, for
+    * example, one of the override specifiers that also works under /clr.
+    */
+    //# pragma warning(disable : 4481)
+#endif
---
+++ bowUtilities/include/requisites/bowUtilitiesRequisites.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "bowPlatformDefines.h"
+#include "bowPlatformTypes.h"
