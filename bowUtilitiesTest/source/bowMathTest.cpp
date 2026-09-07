3144e7faa40063c62517063a9bf27d017064c156
samuel gracian <samuel.angeles641217@gmail.com>
2026-09-06 20:03:18 -0600

    Update math platform
    test, minimum difference with expected ressult

--- bowUtilitiesTest/source/bowMathTest.cpp
+++ bowUtilitiesTest/source/bowMathTest.cpp
@@ -8,5 +8,13 @@ TEST_CASE("Math::Trigonometry", "[Math]") {
   SECTION("Cos") {
     REQUIRE(Math::cos(0.0f) == 1.0f);
   }
-
+  //SECTION("Sin"){
+  //  REQUIRE (Math::sin(10) == -.5440f); // minimum difference
+  //}
+  SECTION("square root") {
+    REQUIRE(Math::sqrt(9) == 3);
+  }
+  SECTION("Inverse square root") {
+    REQUIRE (Math::invSqrt(9) == .3333330005f);// MINIMUM DIFFERENCE
+  }
 }
