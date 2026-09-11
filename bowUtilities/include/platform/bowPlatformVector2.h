bab644e1f232be6df25f0a57e270c39513e5be0c
samuel gracian <samuel.angeles641217@gmail.com>
2026-09-11 17:44:42 -0600

    Update vector2. Added corss product, magnitude, square magnitude, squr distance as functions

--- bowUtilities/include/platform/bowPlatformVector2.h
+++ bowUtilities/include/platform/bowPlatformVector2.h
@@ -8,27 +8,72 @@ class BOW_UTILITIES_EXPORT Vector2
 public:
 
   /*
-  * @brief Defautl constructor.
+  * @brief
+     Defautl constructor.
   */
   Vector2() = default;
 
   /*
-  * @brief Constructor with given values.
+  * @brief
+     Constructor with given values.
   */
   Vector2(float X, float Y);
 
  /*
- * @brief Default destructor.
+ * @brief
+    Default destructor.
  */
   ~Vector2() = default;
 
   /*
-  * @brief Dot product from two vector 2.
-  * @param Vector2 The vector2 to calculate the dot product with.
-  * @return float The dot product from this vector2 and the param.
+  * @brief
+     Dot product from two vector 2.
+  * @param Vector2
+     Vector2 The vector2 to calculate the dot product with.
+  * @return float
+     float The dot product from this vector2 and the param.
   */
   float
-  dot(const Vector2& vector2) const;
+  dot(Vector2& vector2);
+
+  /*
+  * @brief 
+  *  Calculates the cross product from 2 vectors.
+  * @Param Vector2
+  * @return float
+  *  Corss product from 2 vectors.
+  */
+  float
+  cross(Vector2& vector2);
+
+  /*
+  * @brief
+  *  Calculates the magnitude (lenght of the vector).
+  * @return float
+  *  The lenght of the vector.
+  */
+  float
+  magnitude() const;
+
+  /*
+  * @brief
+  *  Calculates the square magnitude (lengh) of this vector.
+  * @return float
+  *  The square lenght of this vector.
+  */
+  float
+  sqrMagnitude() const;
+  
+  /*
+  * @brief
+  *  Calculates the square distence betweeen 2 vectors.
+  * @param Vector2
+  *  The vector to calculate the square distance to.
+  * @return float
+  *  The square distance between this vector and the one given as param.
+  */
+  float
+  sqrDistance(Vector2 vector) const;
 
 private:
   float m_x;
--- bowUtilities/source/platform/bowPlatformVector2.cpp
+++ bowUtilities/source/platform/bowPlatformVector2.cpp
@@ -1,5 +1,7 @@
 #include "bowPlatformVector2.h"
 
+#include "bowMath.h"
+
 namespace bowEngineSDK
 {
 Vector2::Vector2(float X, float Y)
@@ -8,7 +10,29 @@ Vector2::Vector2(float X, float Y)
 }
 
 float
-Vector2::dot(const Vector2& vector2) const {
+Vector2::dot(Vector2& vector2){
   return (m_x * vector2.m_x) + (m_y * vector2.m_y);
+}
+
+float
+Vector2::cross(Vector2& vector2) {
+  return (m_x * vector2.m_y) - (m_y * vector2.m_x);
+}
+
+float
+Vector2::magnitude()const{
+  return Math::sqrt(sqrMagnitude());
+}
+
+float
+Vector2::sqrMagnitude()const{
+  return Math::pow(m_x, 2) * Math::pow(m_y, 2);
+}
+
+float
+Vector2::sqrDistance(Vector2 vector) const {
+  const float deltaX = m_x - vector.m_x;
+  const float deltaY = m_y - vector.m_y;
+  return (deltaX * deltaX) + (deltaY * deltaY);
 }
 }
