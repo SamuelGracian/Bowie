f95e94bebb7244ecbbc3ba8a6f05cda0056b0efc
samuel gracian <samuel.angeles641217@gmail.com>
2026-09-23 19:01:03 -0600

    fixed indentation

--- bowUtilities/source/platform/bowVector3.cpp
+++ bowUtilities/source/platform/bowVector3.cpp
@@ -29,15 +29,13 @@ Vector3::Vector3(const Vector3& vector)
 Vector3
 Vector3::cross(const Vector3& vector) const {
   return Vector3 (y * vector.z - z * vector.y,
-          z * vector.x - x * vector.z,
-          x * vector.y - y * vector.x);
+                  z * vector.x - x * vector.z,
+                  x * vector.y - y * vector.x);
 }
 
 float
 Vector3::dot(const Vector3& vector) const {
-  return (x * vector.x
-          + y * vector.y
-          + z * vector.z);
+  return (x * vector.x + y * vector.y + z * vector.z);
 }
 
 float
